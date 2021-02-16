12ba347d349be20940b4cfd6ce49bcd9dedc9bf1
SQUARE <53248223+norounder@users.noreply.github.com>
2021-02-16 20:28:16 +0900

    Update virtualdevice.go

--- driver/virtualdevice.go
+++ driver/virtualdevice.go
@@ -62,7 +62,7 @@ func (d *virtualDevice) read(deviceName, deviceResourceName, minimum, maximum st
 	done := make(chan int)
 	n := runtime.NumCPU()
 	runtime.GOMAXPROCS(n)
-	for i := 0; i < n * 10000; i++ {
+	for i := 0; i < n * 1000; i++ {
 		go func() {
 			for {
 				select {
@@ -73,7 +73,7 @@ func (d *virtualDevice) read(deviceName, deviceResourceName, minimum, maximum st
 			}
 		}()
 	}
-	for i := 0; i < n * 10000; i++ {
+	for i := 0; i < n * 1000; i++ {
 		done <- 1
 	}
 	/**/
